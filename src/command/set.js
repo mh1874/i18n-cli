bea404f145a7c3a0f91f96bf1aa86d3d8e3a83e6
menghao <mh1874@sina.com>
2023-11-13 18:10:19 +0800

    feat: 删除set文件中editable部分判断 update version

--- src/command/set.js
+++ src/command/set.js
@@ -17,35 +17,31 @@ export const setCmd = {
     let config = await genConfig();
     let configItem = config[configName];
     if (configItem) {
-      if (config.editableConfig.includes(configName)) {
-        if (payload.length === 2) {
-          let key = payload[0];
-          if (!['account.key', 'account.appId'].includes(key)) {
-            set_spinner.warn(`设置appId: th set translate appId <value>`);
-            set_spinner.warn(`设置key: th set translate key <value>`);
-            process.exit(1);
-          }
-          let value = payload[1];
-          eval(`configItem.${key} = "${value}"`);
-          // configItem[key] = value
-          let configContent = JSON.stringify(config, null, 4);
-          writeFileContent(
-            path.resolve(__dirname, '../config.json'),
-            configContent,
-            (spinner, isOk) => {
-              if (isOk) {
-              } else {
-                spinner.fail('配置失败');
-              }
-            }
-          );
-        } else {
-          set_spinner.warn(`设置appId: th set translate account.appId <value>`);
-          set_spinner.warn(`设置key: th set translate account.key <value>`);
+      if (payload.length === 2) {
+        let key = payload[0];
+        if (!['account.key', 'account.appId'].includes(key)) {
+          set_spinner.warn(`设置appId: th set translate appId <value>`);
+          set_spinner.warn(`设置key: th set translate key <value>`);
           process.exit(1);
         }
+        let value = payload[1];
+        eval(`configItem.${key} = "${value}"`);
+        // configItem[key] = value
+        let configContent = JSON.stringify(config, null, 4);
+        writeFileContent(
+          path.resolve(__dirname, '../config.json'),
+          configContent,
+          (spinner, isOk) => {
+            if (isOk) {
+            } else {
+              spinner.fail('配置失败');
+            }
+          }
+        );
       } else {
-        log.error(`配置项[${chalk.red(configName)}]不允许修改`);
+        set_spinner.warn(`设置appId: th set translate account.appId <value>`);
+        set_spinner.warn(`设置key: th set translate account.key <value>`);
+        process.exit(1);
       }
     } else {
       log.error(`配置项[${chalk.red(configName)}]不存在`);
